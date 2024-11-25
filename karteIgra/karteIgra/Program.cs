11fc16c4e13bc67b1dd9a11c929b8b5c5b6c8a68
Evian-dev <evian.rebic@gmail.com>
2024-11-25 13:41:11 +0100

    Add files via upload

---
+++ karteIgra/karteIgra/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace karteIgra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int karte = 0;
+            int cardCounter = 0;
+            Console.WriteLine("Unesi kartu između 1 i 13");
+            while (cardCounter < 31)
+            {
+                karte = Convert.ToInt32(Console.ReadLine());
+                if(karte <= 13 && karte >= 1) {
+                    cardCounter += karte;
+                }
+                else
+                {
+
+                    Console.WriteLine("Unesi ispravnu kartu od 1 do 13");
+                }
+
+
+
+
+
+            }
+            if (cardCounter == 31)
+            {
+                Console.WriteLine("Pobjeda");
+
+            }
+            else if (cardCounter > 31)
+                    {
+            Console.WriteLine("Gubitak");
+
+            }
+
+            Console.ReadKey();
+        }
+
+     
+       
+          
+     
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
